2e049f9cfc1f7ba5cd5642527993e0087247ad88
Gustaf Räntilä <g.rantila@gmail.com>
2015-12-14 11:57:55 +0100

    Added benchmark scripts

---
+++ benchmark/index.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var child_process = require( 'child_process' );
+var rimraf        = require( 'rimraf-promise' );
+
+var libs = [
+	'google-libphonenumber',
+	'node-phonenumber',
+	'awesome-phonenumber'
+];
+
+function runCommand( cmd, args, opts )
+{
+	var buffers = [ ];
+	return new Promise( function( resolve, reject ) {
+		var cp = child_process.spawn( cmd, args );
+		cp.stdout.on( 'data', data => {
+			if ( opts && opts.out )
+				buffers.push( data );
+		} );
+		cp.on( 'close', code => {
+			if ( code === 0 )
+			{
+				if ( opts && opts.out )
+					return resolve( Buffer.concat( buffers ).toString( ) );
+				else
+					return resolve( );
+			}
+			reject( new Error( cmd + " exited with exitcode " + code ) );
+		} );
+		cp.on( 'error', err => reject( err ) );
+	} );
+}
+
+function test( lib )
+{
+	return new Promise( ( resolve, reject ) => {
+		var cp = child_process.spawn( 'node', [ 'test.js', lib ] );
+
+		cp.stdout.on( 'data', data => {
+			// Assume only one 'data' packet, I don't really care
+			resolve( JSON.parse( data.toString( ) ) );
+		} );
+
+		cp.stdout.on( 'error', err =>
+			reject( new Error( err ) )
+		);
+	} );
+}
+
+function install_all( )
+{
+	return runCommand( 'npm', [ 'install' ] );
+}
+
+var iterations = 50;
+
+function run_tests( lib )
+{
+	console.log( `Testing speeds of ${lib}` );
+
+	var results = [ ];
+	var iteration = iterations;
+
+	function test_speeds( )
+	{
+		return test( lib )
+		.then( result => {
+			if ( results.length === 0 )
+				results = result;
+			else
+			{
+				results[ 0 ] += result[ 0 ];
+				results[ 1 ] += result[ 1 ];
+				results[ 2 ] += result[ 2 ];
+			}
+		} )
+		.then( _ => {
+			if ( --iteration > 0 )
+				return test_speeds( );
+		} );
+	}
+
+	function install_test( )
+	{
+		var iterations = 5;
+		var iteration = iterations;
+		var time = 0;
+
+		function recurse( )
+		{
+			return rimraf( './node_modules' )
+			.then( _ => {
+				var start = Date.now( );
+				return runCommand( 'npm', [ 'install', lib ] )
+				.then( _ => {
+					var end = Date.now( );
+					time += ( end - start );
+				} );
+			} )
+			.then( _ => {
+				if ( --iteration )
+					return recurse( );
+			} );
+		}
+
+		return recurse( )
+		.then( _ => time / iterations )
+		.then( time => {
+			var findArgs = [ `node_modules/${lib}`, '-type', 'f' ];
+			return runCommand( 'find', findArgs, { out: true } )
+			.then( files => {
+				files = files.split( "\n" ).length;
+
+				var duArgs = [ '-hs', `node_modules/${lib}` ];
+				return runCommand( 'du', duArgs, { out: true } )
+				.then( size => {
+					size = size.split( "\t" )[ 0 ];
+					return {
+						time  : time,
+						files : files,
+						size  : size
+					};
+				} );
+			} );
+		} );
+	}
+
+	return install_all( )
+	.then( test_speeds )
+	.then( install_test )
+	.then( data => {
+		console.log( {
+			load  : results[ 0 ] / iterations,
+			parse : results[ 1 ] / iterations,
+			mem   : results[ 2 ] / iterations,
+			time  : data.time,
+			files : data.files,
+			size  : data.size
+		} );
+	} );
+}
+
+Promise.resolve( )
+.then( _ => run_tests( libs[ 0 ] ) )
+.then( _ => run_tests( libs[ 1 ] ) )
+.then( _ => run_tests( libs[ 2 ] ) )
+.catch( err => console.error( err ) );
---
+++ benchmark/test.js
@@ -0,0 +1,54 @@
+
+var libs = {
+	'awesome-phonenumber': function( ) {
+		var mem1 = process.memoryUsage( );
+		var time1 = Date.now( );
+
+		var PhoneNumber = require( 'awesome-phonenumber' );
+
+		var time2 = Date.now( );
+
+		PhoneNumber( '0707123456', 'SE' ).getNumber( );
+
+		var time3 = Date.now( );
+		var mem2 = process.memoryUsage( );
+
+		return [ time2 - time1, time3 - time2, mem2.rss - mem1.rss ];
+	},
+	'google-libphonenumber': function( ) {
+		var mem1 = process.memoryUsage( );
+		var time1 = Date.now( );
+
+		var PNF = require('google-libphonenumber').PhoneNumberFormat;
+		var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
+		var time2 = Date.now( );
+
+		var phoneNumber = phoneUtil.parse('0707123456', 'SE');
+		phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
+
+		var time3 = Date.now( );
+		var mem2 = process.memoryUsage( );
+
+		return [ time2 - time1, time3 - time2, mem2.rss - mem1.rss ];
+	},
+	'node-phonenumber': function( ) {
+		var mem1 = process.memoryUsage( );
+		var time1 = Date.now( );
+
+		var phone = require('node-phonenumber');
+		var phoneUtil = phone.PhoneNumberUtil.getInstance();
+
+		var time2 = Date.now( );
+
+		var phoneNumber = phoneUtil.parse('0707123456','SE');
+		var toNumber = phoneUtil.format(phoneNumber, phone.PhoneNumberFormat.INTERNATIONAL);
+
+		var time3 = Date.now( );
+		var mem2 = process.memoryUsage( );
+
+		return [ time2 - time1, time3 - time2, mem2.rss - mem1.rss ];
+	}
+};
+
+console.log( libs[ process.argv[ 2 ] ]( ) );

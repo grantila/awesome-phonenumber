3873e490b55051e40688af07d6fae8a0899b1e4b
Gustaf Räntilä <g.rantila@gmail.com>
2017-09-04 10:45:38 +0200

    Added TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,57 @@
+
+declare namespace AwesomePhonenumber
+{
+	type PhoneNumberFormat =
+		'e164' |
+		'international' |
+		'national' |
+		'rfc3966' |
+		'significant';
+
+	type PhoneNumberTypes =
+		'fixed-line' |
+		'fixed-line-or-mobile' |
+		'mobile' |
+		'pager' |
+		'personal-number' |
+		'premium-rate' |
+		'shared-cost' |
+		'toll-free' |
+		'uan' |
+		'voip' |
+		'unknown';
+
+
+	class PhoneNumber
+	{
+		constructor( phoneNumber: string, countryCode?: string );
+
+		isValid( ): boolean;
+		canBeInternationallyDialled( ): boolean;
+		isPossible( ): boolean;
+		getType( ): PhoneNumberTypes;
+		isMobile( ): boolean;
+		isFixedLine( ): boolean;
+		getNumber( type?: PhoneNumberFormat ): string;
+		getNumberFrom( regionCode: string ): string;
+		getRegionCode( ): boolean;
+		toJSON( ): any;
+
+		static getCountryCodeForRegionCode( regionCode: string ): number;
+		static getRegionCodeForCountryCode( countryCode: number ): string;
+		static getSupportedCallingCodes( ): string[ ];
+		static getExample( regionCode: string, type?: PhoneNumberTypes ): PhoneNumber;
+		static getAsYouType( regionCode: string ): AsYouType;
+	}
+
+	class AsYouType
+	{
+		addChar( char: string ): string;
+		number( ): string;
+		removeChar( ): string;
+		reset( number?: string ): string;
+		getPhoneNumber( ): PhoneNumber;
+	}
+}
+
+export default AwesomePhonenumber.PhoneNumber;
---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf test
+cp -r test.in test
+
+node_modules/.bin/tsc -p .
+
+node_modules/.bin/mocha

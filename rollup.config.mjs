7d643df9cacda86069b29c6438f29dbbf7b1054c
Gustaf Räntilä <g.rantila@gmail.com>
2023-02-01 20:56:07 +0100

    fix(deps): Bumped dev deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,12 +2,14 @@ import commonjs from '@rollup/plugin-commonjs';
 
 export default [
   {
+    strictDeprecations: true,
     input: './index.js',
     output: {
       file: './build/index-esm.js',
       format: 'es',
       interop: 'auto',
       exports: 'named',
+      esModule: true,
     },
     plugins: [commonjs()],
   },
